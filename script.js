c2ced3e36762e3a8f8006d0101a458323a2fbd81
Shivam Gangwani <shivam.gangwani2804@gmail.com>
2021-11-27 18:53:50 +0530

    Added function game

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     // Plays one round of Rock Paper Scissors
 
-    // player_win records whether player won, lost or tied
+    // Returns player_win, which records whether player won, lost or tied
     // player_win takes 3 values: 0, 1, 2
     // 0 -> loss, 1 -> tie, 2 -> win
     let player_win = 0;
@@ -41,25 +41,25 @@ function playRound(playerSelection, computerSelection) {
     switch(playerSelection) {
 
         case "Rock": 
-            if(computerSelection == "Rock") {
+            if(computerSelection === "Rock") {
                 player_win = 1; // Tie
             }
-            else if(computerSelection == "Paper") {
+            else if(computerSelection === "Paper") {
                 player_win = 0; // Loss
             }
-            else if(computerSelection == "Scissor") {
+            else if(computerSelection === "Scissor") {
                 player_win = 2; // Win
             }
             break;
 
         case "Paper":
-            if(computerSelection == "Rock") {
+            if(computerSelection === "Rock") {
                 player_win = 2; // Win
             }
-            else if(computerSelection == "Paper") {
+            else if(computerSelection === "Paper") {
                 player_win = 1; // Tie
             }
-            else if(computerSelection == "Scissor") {
+            else if(computerSelection === "Scissor") {
                 player_win = 0; // Loss
             }
             break;
@@ -77,10 +77,8 @@ function playRound(playerSelection, computerSelection) {
             break;
     }
 
-    // Map outcome to text
-    let result = map_outcome(player_win, computerSelection, playerSelection);
-
-    return result;
+    // 0 if lost, 1 if tied, 2 if won
+    return player_win;
 }
 
 function capitalize(str) {
@@ -89,7 +87,7 @@ function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
-function map_outcome(outcome, computerSelection, playerSelection) {
+function map_outcome(outcome, playerSelection, computerSelection) {
     // Helper function
     // Maps outcome to text
     // Outcome can be 0 (loss), 1 (tie), 2 (win)
@@ -109,4 +107,52 @@ function map_outcome(outcome, computerSelection, playerSelection) {
         result = `You won! ${playerSelection} beats ${computerSelection}`;
     }
     return result;
+}
+
+
+function game() {
+    // Play 5 rounds of Rock Paper Scissors
+    let score = 0;
+
+    let wins = 0,
+        ties = 0,
+        losses = 0;
+
+    for(let i = 1; i <= 5; i++) {
+        // Play a round
+        
+        // Ask player for their play
+        let playerSelection = prompt(`Round ${i} : Rock, Paper or Scissors? `);
+        let computerSelection = computerPlay();
+
+        // Get result
+        let result = playRound(playerSelection, computerSelection);
+
+        // Show the result
+        console.log("----------------");
+        console.log(`Round ${i} result`);
+        console.log(`You played ${playerSelection}`);
+        console.log(`Computer played ${computerSelection}`);
+        console.log(map_outcome(result, playerSelection, computerSelection));
+        
+        // Show score after current round
+        score += result;
+        console.log(`Your current score: ${score}`);
+        console.log("----------------");
+
+        // Update wins, ties and losses tally
+        wins += (result === 2) ? 1 : 0;
+        ties += (result === 1) ? 1 : 0;
+        losses += (result === 0) ? 1 : 0;
+    }
+
+    // End of game result
+    console.log("----------------");
+    console.log("Game over!");
+    console.log(`Your score: ${score}`);
+    console.log("Total games: 5");
+    console.log(`Games won: ${wins}`);
+    console.log(`Games tied: ${ties}`);
+    console.log(`Games lost: ${losses}`);
+    console.log("----------------");
 }
